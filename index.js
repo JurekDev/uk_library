8306e99185c2e3dea0e3d888eaae3bbfb293d76e
JurekDev <jurek.mueller@outlook.de>
2017-07-20 18:03:47 +0200

    added second dataset

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ var APP_ID = undefined;
 //];
 
 var data =[
-  {
-    libraryName: "Aberdeen Central Library, Local Studies Department", STREET_ADDRESS: "Rosemount Viaduct", cityName: "Aberdeen", PHONE: "01224 652512", gender:"m"
+    {libraryName: "Aberdeen Central Library, Local Studies Department", STREET_ADDRESS: "Rosemount Viaduct", cityName: "Aberdeen", PHONE: "01224 652512", gender:"m"},
+    {libraryName: "Ferryhill Library", STREET_ADDRESS: "Fonthill Road", cityName: "Aberdeen", PHONE: ""},
   }
 ]
 
